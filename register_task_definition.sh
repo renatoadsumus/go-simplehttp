184f09d61d4fc092e822cc6d3b6b86c0b17160d3
root <root@ip-172-31-22-46.ec2.internal>
2020-10-19 19:16:10 +0000

    Incluindo Sript para criar Taskdefinition

--- register_task_definition.sh
+++ register_task_definition.sh
@@ -7,6 +7,6 @@ export TASK_VERSION=$(aws ecs register-task-definition --region us-east-1 --cli-
 
 echo "Registered ECS Task Definition: " $TASK_VERSION
 
-DEPLOYED_SERVICE=$(aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$TASK_VERSION | jq --raw-output '.service.serviceName')
+DEPLOYED_SERVICE=$(aws ecs update-service --region us-east-1 --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$TASK_VERSION | jq --raw-output '.service.serviceName')
 
 echo "Deployment of $DEPLOYED_SERVICE complete"
