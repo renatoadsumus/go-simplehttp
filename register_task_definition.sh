84df9120e8762d1acd3f0cdeb967a7d8749802e6
root <root@ip-172-31-22-46.ec2.internal>
2020-10-19 21:01:15 +0000

    Colocando como Parametro

--- register_task_definition.sh
+++ register_task_definition.sh
@@ -3,10 +3,15 @@
 set -e
 
 
-export TASK_VERSION=$(aws ecs register-task-definition --region us-east-1 --cli-input-json file://container-definitions.json | jq --raw-output '.taskDefinition.revision')
+export TASK_VERSION=$(aws ecs register-task-definition --region us-east-1 --cli-input-json file://container-definitions.yml | jq --raw-output '.taskDefinition.revision')
 
 echo "Registered ECS Task Definition: " $TASK_VERSION
 
-DEPLOYED_SERVICE=$(aws ecs update-service --region us-east-1 --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$TASK_VERSION | jq --raw-output '.service.serviceName')
 
-echo "Deployment of $DEPLOYED_SERVICE complete"
+if [ ! -n "$CLUSTER_NAME" ]
+then
+	DEPLOYED_SERVICE=$(aws ecs update-service --region us-east-1 --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$TASK_VERSION | jq --raw-output '.service.serviceName')
+	echo "Deployment of $DEPLOYED_SERVICE complete"
+else
+	echo "SO FOI CRIADO A TASK DEFINITION"
+fi
